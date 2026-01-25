7367a2fb7ace391ac52e29f7d14206607a1e87b2
Marcin Kaczmarek <mkacz@ik.me>
2026-01-25 22:13:08 +0100

    Rename and clarify WriteStream_safeWrite

--- util/node.ts
+++ util/node.ts
@@ -1,6 +1,13 @@
 import * as fs from "fs"
 
-export async function WriteStream_safeWrite(out: fs.WriteStream, chunk: string): Promise<void> {
+/**
+ * Submit a chunk for writing, ensuring backpressure is handled.
+ *
+ * The returned promise resolves when the chunk has been successfully received
+ * by the stream, but does not indicate anything about whether it has been
+ * flushed to the underlying resource.
+ */
+export async function WriteStream_submit(out: fs.WriteStream, chunk: string): Promise<void> {
 	if (!out.write(chunk)) {
 		return new Promise((resolve, reject) => {
 			out.once("drain", resolve)
